69bdcdd4717a9b69859563ec7bdf824a83e50152
filipecosta90 <filipecosta.90@gmail.com>
2019-12-20 20:16:14 +0000

    [add] bumped version to be above v0.8.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
     name='redisgraph-bulk-loader',
     python_requires='>=3',
-    version='0.0.1',
+    version='0.8.1',
     packages=find_packages(),
     install_requires=[
         'redis',
