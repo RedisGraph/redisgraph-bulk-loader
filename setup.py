6ba80dfdcb3a9b52cc9cd8503bc72443c806bf96
Jeffrey Lovitz <jeffrey.lovitz@gmail.com>
2020-04-29 16:38:21 -0400

    Add setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,18 @@ setup(
         'redis',
         'click'
     ],
+
+    description='RedisGraph Bulk Import Tool',
+    url='https://github.com/redisgraph/redisgraph-bulk-loader',
+    packages=find_packages(),
+    install_requires=['redis', 'click'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.0',
+        'Topic :: Database'
+    ]
+
     entry_points='''
         [console_scripts]
         redisgraph-bulk-loader=redisgraph_bulk_loader.bulk_insert:bulk_insert
