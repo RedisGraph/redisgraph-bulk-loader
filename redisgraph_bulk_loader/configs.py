7b354644e5c094ce94955a01cfc30792a23176a3
Jeffrey Lovitz <jeffrey.lovitz@gmail.com>
2020-05-11 15:08:33 -0400

    wip

--- redisgraph_bulk_loader/configs.py
+++
@@ -1,9 +0,0 @@
-# Default values for command-line arguments
-
-max_token_count = 1024 * 1023
-max_buffer_size = 0
-max_token_size = 512 * 1000000
-skip_invalid_nodes = False
-skip_invalid_edges = False
-separator = ','
-quoting = 3
