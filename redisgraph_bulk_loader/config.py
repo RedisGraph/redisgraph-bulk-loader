e344706430717f4074577478d89987e45a5a42fe
Jeffrey Lovitz <jeffrey.lovitz@gmail.com>
2021-07-12 16:45:27 -0400

    Reduce default values for buffer sizes (#66)

--- redisgraph_bulk_loader/config.py
+++ redisgraph_bulk_loader/config.py
@@ -1,5 +1,5 @@
 class Config:
-    def __init__(self, max_token_count=1024 * 1023, max_buffer_size=2_048, max_token_size=512, enforce_schema=False, skip_invalid_nodes=False, skip_invalid_edges=False, separator=',', quoting=3, store_node_identifiers=False, escapechar='\\'):
+    def __init__(self, max_token_count=1024 * 1023, max_buffer_size=128, max_token_size=128, enforce_schema=False, skip_invalid_nodes=False, skip_invalid_edges=False, separator=',', quoting=3, store_node_identifiers=False, escapechar='\\'):
         """Settings for this run of the bulk loader"""
         # Maximum number of tokens per query
         # 1024 * 1024 is the hard-coded Redis maximum. We'll set a slightly lower limit so
@@ -7,10 +7,10 @@ class Config:
         # ("GRAPH.BULK", "BEGIN", graph name, counts)
         self.max_token_count = min(max_token_count, 1024 * 1023)
         # Maximum size in bytes per query
-        self.max_buffer_size = max_buffer_size * 1_000_000
+        self.max_buffer_size = min(max_buffer_size * 1_000_000, 1024 * 1_000_000)
         # Maximum size in bytes per token
         # 512 megabytes is a hard-coded Redis maximum
-        self.max_token_size = min(max_token_size * 1_000_000, 512 * 1_000_000)
+        self.max_token_size = min(max_token_size * 1_000_000, 512 * 1_000_000, self.max_buffer_size)
 
         self.enforce_schema = enforce_schema
         self.skip_invalid_nodes = skip_invalid_nodes
